e38c29bc4b2218a48555ef7f0c6cbd1080d5f985
joshua-stone <joshua.gage.stone@gmail.com>
2023-01-21 23:52:26 -0500

    Build nvidia kernel modules manually
    The nvidia driver installer will fail to succeed if it detects an
    nvidia card on the host. Building the kernel modules manually should
    avoid this.

--- build-nvidia.sh
+++ build-nvidia.sh
@@ -9,12 +9,31 @@ curl https://download.nvidia.com/XFree86/Linux-x86_64/${NVIDIA_VERSION}/NVIDIA-L
 
 chmod +x ./nvidia-driver.run
 
+# The nvidia driver installer will try to load the nvidia kernel modules after
+# building them if it detects an nvidia card, which will fail in a container
+# build environment. The workaround is to build the kernel modules manually.
+./nvidia-driver.run --extract-only
+
+make -C /NVIDIA-Linux-x86_64-${NVIDIA_VERSION}/kernel -j8
+
+install -D NVIDIA-Linux-x86_64-${NVIDIA_VERSION}/kernel/nvidia{,-{drm,modeset,peermem,uvm}}.ko \
+	--target-directory="/build/usr/lib/modules/${KERNEL_VERSION}/kernel/drivers/video"
+
+date > /.builddate
+
 ./nvidia-driver.run \
     --kernel-name="${KERNEL_VERSION}" \
     --kernel-source-path="/usr/src/kernels/${KERNEL_VERSION}" \
     --x-library-path=/usr/lib64 \
     --x-module-path=/usr/lib64/xorg/modules \
     --no-nouveau-check \
+    --no-precompiled-interface \
+    --no-rpms \
     --skip-depmod \
     --no-questions \
-    --silent
+    --no-distro-scripts \
+    --no-dkms \
+    --no-kernel-module-source \
+    --skip-module-unload \
+    --silent \
+    --no-kernel-modules
