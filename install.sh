7e8cc1789ba15cc6761f6ee2c5cf9753fbaf621d
Joshua Stone <joshua.gage.stone@gmail.com>
2023-03-07 13:13:24 -0500

    feat: split container build into distinct build script stages

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -ouex pipefail
+
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-{cisco-openh264,modular,updates-modular}.repo
+
+install -D /tmp/ublue-os-nvidia-addons/rpmbuild/SOURCES/nvidia-container-runtime.repo \
+    /etc/yum.repos.d/nvidia-container-runtime.repo
+
+source /tmp/akmods/nvidia-vars
+
+rpm-ostree install \
+    xorg-x11-drv-${NVIDIA_PACKAGE_NAME}-{,cuda-,devel-,kmodsrc-,power-}${NVIDIA_FULL_VERSION} \
+    nvidia-container-toolkit nvidia-vaapi-driver \
+    /tmp/akmods/${NVIDIA_PACKAGE_NAME}/kmod-${NVIDIA_PACKAGE_NAME}-${KERNEL_VERSION}-${NVIDIA_AKMOD_VERSION}.fc${RELEASE}.rpm \
+    /tmp/ublue-os-nvidia-addons/rpmbuild/RPMS/noarch/ublue-os-nvidia-addons-*.rpm \
+    /tmp/ublue-os/rpmbuild/RPMS/noarch/ublue-os-just-*.noarch.rpm
