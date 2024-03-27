3b0ab1b90efa30612fb5c72e92c376343649ff26
Benjamin Sherman <benjamin@holyarmy.org>
2024-03-26 23:42:42 -0500

    refactor: combine asus and surface builds with nvidia
    This is a WIP commit which shows has the first steps of asus/surface
    main images mostly working in a Containerfile and skipping main/main.
    
    The nvidia parts will be built in a second target.
    
    Github workflows have not yet been touched.

---
+++ nvidia/install.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+set -ouex pipefail
+
+if [[ "${FEDORA_MAJOR_VERSION}" -le 38 ]]; then
+    sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-{cisco-openh264,modular,updates-modular}.repo
+else
+    sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-cisco-openh264.repo
+fi
+
+if [ -n "${RPMFUSION_MIRROR}" ]; then
+    # force use of single rpmfusion mirror
+    echo "Using single rpmfusion mirror: ${RPMFUSION_MIRROR}"
+    sed -i.bak "s%^metalink=%#metalink=%" /etc/yum.repos.d/rpmfusion-*.repo
+    sed -i "s%^#baseurl=http://download1.rpmfusion.org%baseurl=${RPMFUSION_MIRROR}%" /etc/yum.repos.d/rpmfusion-*.repo
+    # after F40 launches, bump to 41
+    if [[ "${FEDORA_MAJOR_VERSION}" -ge 40 ]]; then
+        sed -i "s%free/fedora/releases%free/fedora/development%" /etc/yum.repos.d/rpmfusion-*.repo
+    fi
+fi
+
+rpm-ostree install \
+    /tmp/akmods-rpms/ublue-os/ublue-os-nvidia-addons-*.rpm
+
+source /tmp/akmods-rpms/kmods/nvidia-vars.${NVIDIA_MAJOR_VERSION}
+
+if [[ "${IMAGE_NAME}" == "kinoite" ]]; then
+    VARIANT_PKGS="supergfxctl-plasmoid supergfxctl"
+elif [[ "${IMAGE_NAME}" == "silverblue" ]]; then
+    VARIANT_PKGS="gnome-shell-extension-supergfxctl-gex supergfxctl"
+else
+    VARIANT_PKGS=""
+fi
+
+rpm-ostree install \
+    xorg-x11-drv-${NVIDIA_PACKAGE_NAME}-{,cuda-,devel-,kmodsrc-,power-}${NVIDIA_FULL_VERSION} \
+    xorg-x11-drv-${NVIDIA_PACKAGE_NAME}-libs.i686 \
+    nvidia-container-toolkit nvidia-vaapi-driver ${VARIANT_PKGS} \
+    /tmp/akmods-rpms/kmods/kmod-${NVIDIA_PACKAGE_NAME}-${KERNEL_VERSION}-${NVIDIA_AKMOD_VERSION}.fc${RELEASE}.rpm
+
+if [ -n "${RPMFUSION_MIRROR}" ]; then
+    # reset forced use of single rpmfusion mirror
+    echo "Revert from single rpmfusion mirror: ${RPMFUSION_MIRROR}"
+    rename -v .repo.bak .repo /etc/yum.repos.d/rpmfusion-*repo.bak
+fi
