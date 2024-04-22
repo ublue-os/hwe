a0f9806327e28c810d38aac7d2077268e4ab6ce9
Robert Sturla <robertsturla@outlook.com>
2024-04-22 23:25:03 +0100

    Use proper RPMFusion repositories for F40 builds

--- install.sh
+++ install.sh
@@ -9,8 +9,8 @@ if [ "${KERNEL_FLAVOR}" = "main" ]; then
     exit 0
 fi
 
-# after F40 launches, bump to 41
-if [[ "${FEDORA_MAJOR_VERSION}" -ge 40 ]]; then
+# after F41 launches, bump to 42
+if [[ "${FEDORA_MAJOR_VERSION}" -ge 41 ]]; then
     # note: this is done before single mirror hack to ensure this persists in image and is not reset
     # pre-release rpmfusion is in a different location
     sed -i "s%free/fedora/releases%free/fedora/development%" /etc/yum.repos.d/rpmfusion-*.repo
